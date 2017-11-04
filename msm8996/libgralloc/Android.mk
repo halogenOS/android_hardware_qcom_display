eaba6613caaf41457341f5ad68ff40952e0566a0
Simao Gomes Viana <xdevs23@outlook.com>
2017-11-04 18:49:46 +0100

    msm8996: libgralloc: depend on libqdutils
    Change-Id: I47b94988a54c0c945309420a2509ed8feb225342
    Signed-off-by: Simao Gomes Viana <xdevs23@outlook.com>

--- msm8996/libgralloc/Android.mk
+++ msm8996/libgralloc/Android.mk
@@ -27,8 +27,8 @@ LOCAL_MODULE                  := gralloc.$(TARGET_BOARD_PLATFORM)
 LOCAL_MODULE_RELATIVE_PATH    := hw
 LOCAL_PROPRIETARY_MODULE      := true
 LOCAL_MODULE_TAGS             := optional
-LOCAL_C_INCLUDES              := $(common_includes) $(kernel_includes)
-LOCAL_SHARED_LIBRARIES        := $(common_libs) libmemalloc libqdMetaData
+LOCAL_C_INCLUDES              := $(common_includes) $(kernel_includes) hardware/qcom/display/$(TARGET_BOARD_PLATFORM)/libqdutils
+LOCAL_SHARED_LIBRARIES        := $(common_libs) libmemalloc libqdMetaData libqdutils
 LOCAL_SHARED_LIBRARIES        += libGLESv1_CM
 LOCAL_CFLAGS                  := $(common_flags) -DLOG_TAG=\"qdgralloc\" -Wno-sign-conversion
 LOCAL_CLANG                   := true
