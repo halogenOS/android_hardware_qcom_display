97c8a3929c84c56074fb30b258c071e1df33d1e3
Simao Gomes Viana <xdevs23@outlook.com>
2017-11-04 18:11:09 +0100

    msm8996: libcopybit: depend on libgralloc
    Change-Id: I0546e3b3c2e52927b31986ec074f8df5707b57c4

--- msm8996/libcopybit/Android.mk
+++ msm8996/libcopybit/Android.mk
@@ -27,7 +27,7 @@ LOCAL_MODULE_RELATIVE_PATH    := hw
 LOCAL_PROPRIETARY_MODULE      := true
 LOCAL_MODULE_TAGS             := optional
 LOCAL_C_INCLUDES              := $(common_includes) $(kernel_includes)
-LOCAL_SHARED_LIBRARIES        := $(common_libs) libdl
+LOCAL_SHARED_LIBRARIES        := $(common_libs) libdl libgralloc
 LOCAL_CFLAGS                  := $(common_flags) -DLOG_TAG=\"qdcopybit\" -Wno-sign-conversion
 LOCAL_ADDITIONAL_DEPENDENCIES := $(common_deps)
 LOCAL_CLANG                   := true
